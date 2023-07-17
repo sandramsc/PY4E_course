28ba80366aed793246d0ed27a7f0a794df543c90
Sandra Ashipala <sandraashipala@gmail.com>
2023-07-17 13:39:20 +0200

    new exercises from ch. 12

---
+++ exercises/ch12_following_links.py
@@ -0,0 +1,34 @@
+#Use urllib to retrieve the total number of paragraphs
+import urllib.request, urllib.parse, urllib.error
+from bs4 import BeautifulSoup
+import ssl
+
+#Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+url = input('Enter URL: ')
+start_pos = int(input('Enter position: '))
+count = int(input('Enter count: '))
+print("Retrieving:",url)
+for _ in range(count):
+    try:
+        html = urllib.request.urlopen(url, context=ctx).read()
+        soup = BeautifulSoup(html, 'html.parser')
+
+        # Retrieve all of the anchor tags
+        tags = soup('a')
+        if len(tags) == 0:
+             print("No links found.")
+             break
+
+        start_pos = start_pos % len(tags)
+
+        # Gets the next link
+        url = tags[start_pos - 1].get('href', None)
+        print("Retrieving:",url)
+
+    except Exception as e:
+            print("Error fetching content:", e)
+            break
